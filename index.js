eb46a87b6459c5b9bae0e6c5719234a3fd2d67fa
Dawn Carroll <dsc254@gmail.com>
2018-01-14 16:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase)
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase
+    return "I can't hear you!"
+  
+  if string === string.toUpperCase
+    return "YES INDEED!"
+    
+  if string === "I love you, Grandma."
+    return "I love you, too."
+}
